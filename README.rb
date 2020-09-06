c2b8fdf84c1e3300907b6aca9d53184b8c06af75
Nicolas Oga <2112.oga@gmail.com>
2020-09-06 18:36:28 -0300

    Add Readme

---
+++ README.rb
@@ -0,0 +1,10 @@
+# Lawmatics coding challenge
+
+Hopefully, the unit and integration specs are enough documentation about how this little code base works.
+
+~~~bash
+bundle install
+bundler exec rspec
+~~~
+
+Thank you for reviewing!
